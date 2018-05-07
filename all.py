603c40d0578520f0c3762dd69648361749392dcc
GaotianJin <jgaotian@163.com>
2018-05-07 11:40:30 +0800

    修改提交

---
+++ all.py
@@ -0,0 +1,38 @@
+# flag = False
+# name = 'Tom'
+
+# if name == 'Tian':
+#     flag = True
+#     print('welcome boss')
+# else:
+#     print(name)
+
+
+# print('------------------我爱鱼C工作室------------------')
+# temp = input("不妨猜一下小甲鱼现在心里想的是哪个数字：")
+# while temp.strip() == '' or not temp.isdigit():
+#     temp = input('请输入数字：')
+# guess = int(temp)
+# while guess != 8:
+#     if guess > 8:
+#         print('猜错啦！猜的太大了！')
+#     else:
+#         print('猜错啦！猜的太小了！')
+#     temp = input("重新猜一下吧：")
+#     while temp.strip() == '' or not temp.isdigit():
+#         temp = input('请输入数字：')
+#     guess = int(temp)
+# print("我草，你是小甲鱼心里的蛔虫吗？！")
+# print("哼，猜中了也没有奖励！")
+# print("游戏结束，不玩啦^_^")
+# """这是注释"""
+
+# print('这是字符串', '5+8')
+# print('这是数字', 5+8)
+# print('Let\'s go!')
+# 需要原始字符串 前面加r
+# str = r'D:\\game'
+# print(str)
+# print(r'D:\\commit')
+# 换行符
+# print('今天天气不错\n适合出去玩')
